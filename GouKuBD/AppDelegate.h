2915f0de89b05b39e548a98b5b2e6bac3579f1f3
xiao817287 <li817287@gmail.com>
2018-02-26 14:01:40 +0800

    Initial Commit

---
+++ GouKuBD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GouKuBD
+//
+//  Created by lixiao on 2018/2/26.
+//  Copyright © 2018年 lixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GouKuBDTests/GouKuBDTests.m
@@ -0,0 +1,39 @@
+//
+//  GouKuBDTests.m
+//  GouKuBDTests
+//
+//  Created by lixiao on 2018/2/26.
+//  Copyright © 2018年 lixiao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GouKuBDTests : XCTestCase
+
+@end
+
+@implementation GouKuBDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
